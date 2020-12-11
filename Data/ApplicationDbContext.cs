27d7cf409a23df51f52c1670c0b9bd634e3e8956
saba024 <naskidashvili.2001@yahoo.com>
2020-12-11 21:14:48 +0300

    update login

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using chatbot.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,7 @@ namespace chatbot.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<TelegramUser> TelegramUser { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
